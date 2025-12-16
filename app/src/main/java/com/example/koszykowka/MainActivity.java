3e630a2b4b73151c9ee2cbf987f9e3a637138252
asdfghjkl <zurawkalukasz9@gmail.com>
2025-12-16 11:32:22 +0100

    livedata

--- app/src/main/java/com/example/koszykowka/MainActivity.java
+++ app/src/main/java/com/example/koszykowka/MainActivity.java
@@ -1,24 +1,66 @@
 package com.example.koszykowka;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.koszykowka.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
+    PunktyViewModel punktyViewModel;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        punktyViewModel = new ViewModelProvider(this).get(PunktyViewModel.class);
+
+        punktyViewModel.getPunkty().observe(
+                this,
+                new Observer<Integer>() {
+                    @Override
+                    public void onChanged(Integer integer) {
+                        binding.textView.setText("" + integer);
+                    }
+                }
+        );
+
+        binding.button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        punktyViewModel.addPunkty(1);
+
+                    }
+                }
+        );
+        binding.button2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        punktyViewModel.addPunkty(2);
+
+                    }
+                }
+        );
+        binding.button3.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        punktyViewModel.addPunkty(3);
+
+                    }
+                }
+        );
     }
 }
--- app/src/main/java/com/example/koszykowka/PunktyViewModel.java
+++ app/src/main/java/com/example/koszykowka/PunktyViewModel.java
@@ -1,4 +1,33 @@
 package com.example.koszykowka;
 
-public class PunktyViewModel {
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class PunktyViewModel extends ViewModel {
+    private MutableLiveData<Integer> punkty;
+
+    public MutableLiveData<Integer> getPunkty() {
+        if(punkty==null){
+            punkty = new MutableLiveData<>();
+            punkty.setValue(0);
+        }
+        return punkty;
+    }
+
+    public void setPunkty(MutableLiveData<Integer> punkty) {
+        if(punkty==null){
+            punkty = new MutableLiveData<>();
+            punkty.setValue(0);
+        }
+        this.punkty = punkty;
+    }
+
+    public void addPunkty(int x){
+        if(punkty==null){
+            punkty = new MutableLiveData<>();
+            punkty.setValue(0);
+        }
+        int aktualnePunkty = punkty.getValue();
+        punkty.setValue(aktualnePunkty+x);
+    }
 }
